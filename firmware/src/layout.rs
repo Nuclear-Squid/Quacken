386b7750fad38d0980172c512d9fd2143632d2df
Nuclear Squid <leo@cazenave.cc>
2025-08-03 13:57:06 +0200

    Wip (matrix).

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -1,37 +1,11 @@
-use keyberon::action::{k, m, Action::*, HoldTapAction, HoldTapConfig};
-use keyberon::key_code::KeyCode::*;
-
-// Shift + KeyCode
-#[allow(unused_macros)]
-macro_rules! s {
-    ($k:ident) => {
-        m(&[LShift, $k])
-    };
-}
-
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub enum CustomActions {
-}
-
-// #[rustfmt::skip]
-// pub static LAYERS: keyberon::layout::Layers<12, 4, 1, CustomActions> = [
-    // /* QWERTY */
-    // [
-        // [k(Tab),    k(Q),   k(W),   k(E),   k(R),   k(T),     k(Y),   k(U),   k(I),     k(O),     k(P),      k(BSpace), ],
-        // [k(Escape), k(A),   k(S),   k(D),   k(F),   k(G),     k(H),   k(J),   k(K),     k(L),     k(SColon), k(Enter),  ],
-        // [k(LShift), k(Z),   k(X),   k(C),   k(V),   k(B),     k(N),   k(M),   k(Comma), k(Dot),   k(Slash),  k(LShift), ],
-        // [k(No),     k(No),  k(No), k(LCtrl), k(LGui), k(LAlt), k(Space), k(RAlt),  k(RCtrl), k(No),  k(No),   k(No),   ],
-    // ]
-// ];
-
+// Dummy layout that represents the electrical grid,
+// not the physical layout of the keys
 #[rustfmt::skip]
-pub static LAYERS: keyberon::layout::Layers<8, 6, 1, ()> = keyberon::layout::layout! {
-    { //[···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+···],
-        [  A  A  A  A  A  A  A  A  ],
-        [  A  A  A  A  A  A  A  A  ],
-        [  A  A  A  A  A  A  A  A  ],
-        [  A  A  A  A  A  A  A  A  ],
-        [  A  A  A  A  A  A  A  A  ],
-        [  A  A  A  A  A  A  A  A  ],
+pub static LAYERS: keyberon::layout::Layers<12, 4, 1, ()> = keyberon::layout::layout! {
+    {
+        [  Q  Q  W  E  R  T  Y  U  I  O  P  P  ],
+        [  A  A  S  D  F  G  H  J  K  L  ;  ;  ],
+        [  Z  Z  X  C  V  B  N  M  ,  .  /  /  ],
+        [  Q  Q  W  E  R  T  Y  U  I  O  P  P  ],
     }
 };
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -19,7 +19,6 @@ mod app {
     use crate::layout as kb_layout;
 
     use core::convert::Infallible;
-    use core::mem::MaybeUninit;
 
     use rp2040_hal::{
         self,
@@ -32,10 +31,11 @@ mod app {
         gpio,
     };
 
-    // Import the InputPin and OutputPin trait 
+    // Import the InputPin and OutputPin trait
     // XXX rust-analyzer reports that OutputPin is used, but InputPin is not
     use embedded_hal::digital::v2::InputPin;
     use embedded_hal::digital::v2::OutputPin;
+
     // rp2040 implementations of the embedded_hal::digital::v2::{InputPin,OutputPin} traits
     type KbInputPin  = gpio::Pin<gpio::DynPinId, gpio::FunctionSioInput,  gpio::PullUp>;
     type KbOutputPin = gpio::Pin<gpio::DynPinId, gpio::FunctionSioOutput, gpio::PullDown>;
@@ -56,6 +56,7 @@ mod app {
 
     type UsbClass = keyberon::Class<'static, UsbBus, ()>;
     type UsbDevice = usb_device::device::UsbDevice<'static, UsbBus>;
+    type BuiltinLedPin = gpio::Pin<gpio::bank0::Gpio25, gpio::FunctionSioOutput, gpio::PullDown>;
 
     trait ResultExt<T> {
         fn get(self) -> T;
@@ -83,14 +84,14 @@ mod app {
         #[lock_free]
         watchdog: Watchdog,
         #[lock_free]
-        led_pin: gpio::Pin<gpio::bank0::Gpio25, gpio::FunctionSioOutput, gpio::PullDown>,
+        builtin_led_pin: BuiltinLedPin,
     }
 
     #[local]
     struct Local {
-        matrix: Matrix<KbInputPin, KbOutputPin, 8, 6>,
-        layout: Layout<8, 6, 1, ()>,
-        debouncer: Debouncer<[[bool; 8]; 6]>,
+        matrix: Matrix<KbInputPin, KbOutputPin, 6, 8>,
+        layout: Layout<12, 4, 1, ()>,
+        debouncer: Debouncer<[[bool; 6]; 8]>,
         timer: Timer,
     }
 
@@ -121,7 +122,7 @@ mod app {
         .unwrap();
 
         // XXX debgg (Raspberry Pi Pico)
-        let led_pin = pins.gpio25.into_push_pull_output();
+        let builtin_led_pin = pins.gpio25.into_push_pull_output();
 
         let mut timer = Timer::new(c.device.TIMER, &mut resets, &clocks);
         let mut alarm = timer.alarm_0().unwrap();
@@ -149,22 +150,22 @@ mod app {
         let Ok(matrix) = cortex_m::interrupt::free(move |_cs| {
             Matrix::new(
                 [
-                    pins.gpio3.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio4.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio5.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio9.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio18.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio19.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio20.into_pull_up_input().into_dyn_pin(),
-                    pins.gpio10.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio6.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio7.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio8.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio16.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio14.into_pull_up_input().into_dyn_pin(),
+                    pins.gpio15.into_pull_up_input().into_dyn_pin(),
                 ],
                 [
-                    pins.gpio16.into_push_pull_output().into_dyn_pin(),
-                    pins.gpio14.into_push_pull_output().into_dyn_pin(),
-                    pins.gpio15.into_push_pull_output().into_dyn_pin(),
-                    pins.gpio8.into_push_pull_output().into_dyn_pin(),
-                    pins.gpio7.into_push_pull_output().into_dyn_pin(),
-                    pins.gpio6.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio3.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio4.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio5.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio9.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio20.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio19.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio18.into_push_pull_output().into_dyn_pin(),
+                    pins.gpio10.into_push_pull_output().into_dyn_pin(),
                 ],
             )
         });
@@ -175,12 +176,12 @@ mod app {
                 usb_class,
                 alarm,
                 watchdog,
-                led_pin, // XXX debug
+                builtin_led_pin, // XXX debug
             },
             Local {
                 matrix,
                 layout: Layout::new(&kb_layout::LAYERS),
-                debouncer: Debouncer::new([[false; 8]; 6], [[false; 8]; 6], 5),
+                debouncer: Debouncer::new([[false; 6]; 8], [[false; 6]; 8], 5),
                 timer,
             },
             init::Monotonics(),
@@ -208,7 +209,7 @@ mod app {
         binds = TIMER_IRQ_0,
         priority = 1,
         local = [matrix, layout, debouncer, timer],
-        shared = [led_pin, alarm, watchdog, usb_dev, usb_class],
+        shared = [builtin_led_pin, alarm, watchdog, usb_dev, usb_class],
     )]
     fn process_kbd_events(mut c: process_kbd_events::Context) {
         c.shared.alarm.lock(|a| {
@@ -221,18 +222,18 @@ mod app {
         c.shared.watchdog.feed();
 
         for event in c.local.debouncer.events(c.local.matrix.get().get()) {
-            c.local.layout.event(event);
-            // // Quacken Zero: Fit the 4*12 logical layout into the 8*6 physical matrix
-            // let physical_key = {
-            //     if event.coord().0 >= 4 {
-            //         event.transform(|row, col| (row - 4, col))
-            //     }
-            //     else {
-            //         event.transform(|row, col| (row, 11 - col))
-            //     }
-            // };
-            //
-            // c.local.layout.event(physical_key);
+            // Quacken Zero: Fit the 4*12 logical layout into the 8*6 physical matrix
+            let col_to_row = event.transform(|row, col| (col, row));
+            let physical_key = {
+                if col_to_row.coord().0 >= 4 {
+                    col_to_row.transform(|row, col| (row - 4, 11 - col))
+                }
+                else {
+                    col_to_row.transform(|row, col| (row, col))
+                }
+            };
+
+            c.local.layout.event(physical_key);
         }
 
         c.local.layout.tick();
@@ -241,7 +242,6 @@ mod app {
             return;
         }
 
-        // XXX No matter what we do above, this always fails
         let report: KbHidReport = c.local.layout.keycodes().collect();
         if !c
             .shared
@@ -251,6 +251,11 @@ mod app {
             return;
         }
 
+        while let Ok(0) = c.shared.usb_class.lock(|k| k.write(report.as_bytes())) {}
+    }
+
+    #[allow(dead_code)]
+    fn blink_led(led: &mut BuiltinLedPin) {
         unsafe { // XXX dirty debugging: blink the Pico LED
             static mut LED_TURNED_ON: bool = false;
             static mut LED_COUNTER: u32 = 100;
@@ -258,9 +263,9 @@ mod app {
             if LED_COUNTER == 0 {
                 LED_COUNTER = 100;
                 if LED_TURNED_ON {
-                    c.shared.led_pin.set_low().unwrap();
+                    led.set_low().unwrap();
                 } else {
-                    c.shared.led_pin.set_high().unwrap();
+                    led.set_high().unwrap();
                 }
                 LED_TURNED_ON = !LED_TURNED_ON;
             }
@@ -268,7 +273,5 @@ mod app {
                 LED_COUNTER -= 1;
             }
         }
-
-        while let Ok(0) = c.shared.usb_class.lock(|k| k.write(report.as_bytes())) {}
     }
 }
